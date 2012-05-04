4bcdd07ead34b090a4e1ac9a386abd58764e71cd
anhtuan9288@gmail.com <anhtuan9288@gmail.com@a96980ff-dc68-2c34-a4c5-f148e081a36f>
2012-05-04 09:06:29 +0000

    ...

---
+++ trunk/utils.h
@@ -0,0 +1,82 @@
+/*
+ * utils.h
+ *
+ *  Created on: May 4, 2012
+ *      Author: nhatuan
+ */
+
+#ifndef UTILS_H_
+#define UTILS_H_
+
+float ** calculateCov(float *x, float *y, float xm, float ym) {
+	int length = x.length;
+	float ** returnArray = new float*[2];
+	for(int i = 0; i<2; i++) {
+		returnArray[i] = new float[2];
+		for(int j=0; j<2; j++) {
+			returnArray[i][j] = 0;
+		}
+	}
+
+	for(int i=0; i<length; i++) {
+		returnArray[0][0] = (x[i]-xm) * (x[i]-xm);
+		returnArray[0][1] = (x[i]-xm) * (y[i]-xm);
+		returnArray[1][0] = (x[i]-xm) * (y[i]-xm);
+		returnArray[1][1] = (y[i]-xm) * (y[i]-xm);
+	}
+
+	for(int i = 0; i<2; i++) {
+		for(int j=0; j<2; j++) {
+			returnArray[i][j] /= (length - 1);
+		}
+	}
+
+	return returnArray;
+}
+
+float ** calculateCor(float ** cov) {
+	float a = sqrt(cov[0][0]);
+	float b = sqrt(cov[1][1]);
+
+	float ** returnArray = new float*[2];
+	for(int i = 0; i<2; i++) {
+		returnArray[i] = new float[2];
+		for(int j=0; j<2; j++) {
+			returnArray[i][j] = 0;
+		}
+	}
+
+	returnArray[0][0] = cov[0][0] / (a * a);
+	returnArray[0][1] = cov[0][1] / (a * b);
+	returnArray[1][0] = cov[1][0] / (a * b);
+	returnArray[1][1] = cov[1][1] / (b * b);
+
+	return returnArray;
+}
+
+float calculateDet(float ** mat) {
+	return (float) mat[0][0] * mat[1][1] - mat[0][1] * mat[1][0];
+}
+
+float ** calculateInv(float ** mat) {
+	float ** returnArray = new float*[2];
+	for(int i = 0; i<2; i++) {
+		returnArray[i] = new float[2];
+		for(int j=0; j<2; j++) {
+			returnArray[i][j] = 0;
+		}
+	}
+	float det = calculateDet(mat);
+	if (det == 0) return returnArray;
+	float factor = 1/det;
+
+	returnArray[0][0] = factor * mat[1][1];
+	returnArray[1][0] = -factor * mat[1][0];
+	returnArray[0][1] = -factor * mat[0][1];
+	returnArray[1][1] = factor * mat[0][0];
+
+	return returnArray;
+}
+
+
+#endif /* UTILS_H_ */
