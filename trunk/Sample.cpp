5f6d9b4cf84d49157bc328ba480a3d087a007628
anhtuan9288@gmail.com <anhtuan9288@gmail.com@a96980ff-dc68-2c34-a4c5-f148e081a36f>
2012-05-04 07:54:56 +0000

    initial commit

---
+++ trunk/Sample.cpp
@@ -0,0 +1,46 @@
+/*
+ * Sample.cpp
+ *
+ *  Created on: May 4, 2012
+ *      Author: nhatuan
+ */
+
+#include "Sample.h"
+
+Sample::Sample() {
+	// TODO Auto-generated constructor stub
+	name = "";
+	xIntensity = 0;
+	yIntensity = 0;
+}
+
+Sample::Sample(string _name, float x, float y) {
+	name = _name;
+	xIntensity = x;
+	yIntensity = y;
+}
+
+Sample::~Sample() {
+	// TODO Auto-generated destructor stub
+}
+
+void Sample::setName(string _name) {
+	name = _name;
+}
+
+void Sample::setIntensities(float x, float y) {
+	xIntensity = x;
+	yIntensity = y;
+}
+
+string Sample::getName() {
+	return name;
+}
+
+float Sample::getXIntensity() {
+	return xIntensity;
+}
+
+float Sample::getYIntensity() {
+	return yIntensity;
+}
